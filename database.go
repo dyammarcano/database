3cb6f0bce3b5dff02ab1eccd9bcc7c3efbc8cd12
Dyam Antonio Marcano Daniels <dyam.daniels@b3.com.br>
2023-10-27 19:52:00 -0300

    add sql server and db2 still not working

--- database.go
+++ database.go
@@ -4,11 +4,15 @@ import (
 	"database/sql"
 	"database/sql/driver"
 	"fmt"
+	_ "github.com/IBM/go_ibm_db"
+	_ "github.com/denisenkom/go-mssqldb"
 	"github.com/godror/godror"
 	_ "github.com/godror/godror"
+	"gorm.io/driver/db2"
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
+	"gorm.io/driver/sqlserver"
 	"gorm.io/gorm"
 	"time"
 )
@@ -48,6 +52,12 @@ func (db *database) Connect(config *Config) (*gorm.DB, error) {
 	case "oracle":
 		params := connectParamsFromConfig(config)
 		dialector = postgres.New(postgres.Config{Conn: sql.OpenDB(params)})
+	case "sqlserver":
+		dsn := fmt.Sprintf("sqlserver://%s:%s@%s:%d?database=%s", config.User, config.Pass, config.Host, config.Port, config.Name)
+		dialector = sqlserver.Open(dsn)
+	case "db2":
+		dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.Host, config.Port, config.User, config.Pass, config.Name)
+		dialector = db2.Open(dsn)
 	default:
 		return nil, fmt.Errorf("invalid database driver: %s", config.DriverName)
 	}
@@ -79,80 +89,3 @@ func connectParamsFromConfig(cfg *Config) driver.Connector {
 
 	return godror.NewConnector(params)
 }
-
-//package database
-//
-//import (
-//	"context"
-//	"database/sql"
-//	"fmt"
-//	"github.com/godror/godror"
-//	"time"
-//)
-//
-//type (
-//	ConnectionFunc func(config *Config) (*sql.DB, error)
-//
-//	Database interface {
-//		Connect(config *Config) (*sql.DB, error)
-//	}
-//
-//	Config struct {
-//		DriverName string
-//		FilePath   string
-//		User       string
-//		Pass       string
-//		Host       string
-//		Name       string
-//		Port       int
-//	}
-//
-//	database struct {
-//		db  *sql.DB
-//		ctx context.Context
-//	}
-//)
-//
-//var connections = map[string]ConnectionFunc{
-//	"oracle":   connectOracle,
-//	"Postgres": connectPostgres,
-//	"mysql":    connectMySQL,
-//	"sqlite":   connectSQLite,
-//}
-//
-//func NewDatabase() Database {
-//	return &database{}
-//}
-//
-//func (db *database) Connect(config *Config) (*sql.DB, error) {
-//	connFunc, ok := connections[config.DriverName]
-//	if !ok {
-//		return nil, fmt.Errorf("invalid database driver: %s", config.DriverName)
-//	}
-//	return connFunc(config)
-//}
-//
-//func connectOracle(cfg *Config) (*sql.DB, error) {
-//	var params godror.ConnectionParams
-//	params.Username = cfg.User
-//	params.Password = godror.NewPassword(cfg.Pass)
-//	params.ConnectString = fmt.Sprintf("%s:%d/%s?connect_timeout=2", cfg.Host, cfg.Port, cfg.Name)
-//	params.SessionTimeout = 42 * time.Second
-//	params.Timezone = time.Local
-//
-//	return sql.OpenDB(godror.NewConnector(params)), nil
-//}
-//
-//func connectPostgres(cfg *Config) (*sql.DB, error) {
-//	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", cfg.Host, cfg.Port, cfg.User, cfg.Pass, cfg.Name)
-//	return sql.Open("postgres", dsn)
-//}
-//
-//func connectMySQL(cfg *Config) (*sql.DB, error) {
-//	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", cfg.User, cfg.Pass, cfg.Host, cfg.Port, cfg.Name)
-//	return sql.Open("mysql", dsn)
-//}
-//
-//func connectSQLite(cfg *Config) (*sql.DB, error) {
-//	return sql.Open("sqlite3", cfg.FilePath)
-//}
